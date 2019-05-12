7d02ce506375ddf74c138ff0f12298f287607b3d
TimChen <timchen@51donate.com>
2019-05-12 14:13:08 +0800

    test

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -6,13 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloApplication extends SpringBootServletInitializer {
-
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(HelloApplication.class);
-
-  }
+public class HelloApplication{
 
   public static void main(String[] args) {
     SpringApplication.run(HelloApplication.class, args);
